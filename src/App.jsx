7d28d86834c9fed907e87d0b7fcedc28dd88a4f0
suthanRcs <suthanmsd07@gmail.com>
2025-06-24 12:04:08 +0530

    suthan first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      
+      <p className='secondary-para-tag'>Choosing the right institution for online learning is crucial for academic success and career advancement. Botho University stands out as a premier choice for online learners, offering a blend of technological innovation, comprehensive support, and a commitment to excellence. Here's why Botho University should be your top pick for online education:</p>
     </>
   )
 }
