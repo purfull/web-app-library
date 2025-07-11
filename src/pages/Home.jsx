462e9383247b644dc2e86376518c85ae0d38727f
Kavi-R05 <kaviyanagavalli05@gmail.com>
2025-07-11 23:12:30 +0530

    Cards updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,44 +17,43 @@ import EnquireSection from "../components/EnquireSection/EnquireSection";
 const HomePage = () => {
   const cardData = [
     {
-      
       // /images/courses-images/BBA in bus mgmt.jpg
-      cover: "/images/Rectangle 1.png",
+      cover: "/images/courses-images/Bcom in accounting.png",
       courseName: "Undergraduate Programme",
       title: "Bachelor of Commerce in Accounting",
       description:
         "Your Future in Accounting Starts Here! Our Bachelor of Commerce in Accounting programme offers a practical, industry-focused approach to a rewarding career in accounting and finance. You'll gain essential expertise in financial reporting, auditing, taxation, and management accounting. What sets this degree apart is its blend of theory and hands-on experience, including a compulsory semester-long internship for invaluable real-world exposure.",
     },
     {
-      cover: "/images/Rectangle 2.png",
+      cover: "/images/courses-images/MCom in Human resource.png",
       courseName: "Postgraduate Programme",
       title: "Master of Commerce in Human Resource Management.",
       description:
         "Looking to become a leader in talent management? Our Master of Commerce in Human Resource Management (MCom in HRM) is a transformative qualification designed for the modern business landscape. You'll gain a robust foundation in strategic HR planning, recruitment, performance assessment, and more.",
     },
     {
-      cover: "/images/Rectangle 3.png",
+      cover: "/images/courses-images/BBA in bus mgmt.png",
       courseName: "Undergraduate Programme",
       title: "Bachelor of Business Administration in Business Management",
       description:
         "Aiming for a leadership role in the global business environment? Our Bachelor of Business Administration in Business Management provides the strategic mindset and skills you need. This programme covers key areas like management, finance, marketing, entrepreneurship, operations, and digital transformation, with a strong focus on innovation, sustainability, and ethical leadership.",
     },
     {
-      cover: "/images/Rectangle 4.png",
+      cover: "/images/courses-images/Bcom in Hospitality.png",
       courseName: "Undergraduate Programme",
       title: "Bachelor of Commerce in Hospitality Management",
       description:
         "Aspiring to a leadership role in the vibrant hospitality sector? Our Bachelor of Commerce in Hospitality Management provides a strong foundation in business principles alongside specialised hospitality skills. You'll cover core areas like accounting and marketing, combined with modules in hotel operations, food and beverage management, and tourism development.",
     },
     {
-      cover: "/images/Rectangle 5.png",
+      cover: "/images/courses-images/MBA.png",
       courseName: "Postgraduate Programme",
       title: "Master of Business Administration (MBA)",
       description:
         "Aiming to become a top executive in the global business arena? Our MBA programme is designed for ambitious professionals. It equips you with the skills to tackle complex challenges, focusing on critical thinking, entrepreneurship, financial resilience, and innovative problem-solving.",
     },
     {
-      cover: "/images/Rectangle 6.png",
+      cover: "/images/courses-images/Diploma in Occup.png",
       courseName: "Undergraduate Programme",
       title: "Diploma in Occupational Health Safety",
       description:
